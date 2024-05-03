71b6ef049cf6bbb5fdc1bfb10eafd1dcebac55fe
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-05-03 10:31:49 +0530

    Live Chat Added

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -7,11 +7,18 @@ type Props ={
   message: string;
 }
 
-const Cursor = ({ color,x,y,message }: Props) => {
+const Cursor = ({ color, x, y, message }: Props) => {
   return (
     <div className="pointer-events-none absolute top-0 left-0" 
     style={{transform:`translateX(${x}px) translateY(${y}px)`}}>
       <CursorSVG color={color} />
+
+      {message && (
+        <div className="absolute left-0 top-5 rounded-3xl px-4 py-2" style={{backgroundColor: color}}>
+          <p className="text-white whitespace-nowrap text-sm leading-relaxed">{message}</p>
+        </div>
+      )}
+
     </div>
   );
 }
