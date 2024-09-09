29ee60d59f16f3e3125ccd1e769185f92befadb6
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-09-09 23:23:46 +0530

    Navbar and Avatar
    Corrected Errors and form the navbar section and avatars to show live users.

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,19 @@
 import { Room } from "./Room";
 import { CollaborativeApp } from "./CollaborativeApp";
 import Live from "@/components/Live";
+import Navbar from "@/components/Users/Navbar";
+import LeftSideBar from "@/components/LeftSideBar";
+import RightSidebar from "@/components/RightSidebar";
 
 export default function Page() {
   return (
-      <div>
-        <Live />
-      </div>
+      <main className="h-screen overflow-hidden">
+        <Navbar />
+        <section className="flex h-full flex-row">
+          < LeftSideBar />
+          <Live />
+          <RightSidebar />
+        </section>
+      </main>
   );
 }
