0516408222e1acb4710ce1c4216bac94c93f3b30
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-09-19 21:10:38 +0530

    FigPro

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-// Dynamically import Room component (ensure default export)
-const Room = dynamic(() => import('./Room').then(mod => mod.Room), { ssr: false });
+/**
+ * disable ssr to avoid pre-rendering issues of Next.js
+ *
+ * we're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server
+ */
+const App = dynamic(() => import("./App"), { ssr: false });
 
-export default function Page() {
-  return (
-    <div>
-      <Room>
-        <h1 className="text-5xl text-white">Liveblocks Figma Clone</h1>
-      </Room>
-    </div>
-  );
-}
+export default App;
